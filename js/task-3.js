2c3b18417544cd7dca14d1cdb4c291b91188bb1f
Artem Lipchansky <lipchanskiy.an@gmail.com>
2024-01-14 23:17:02 +0200

    added

--- js/task-3.js
+++ js/task-3.js
@@ -1,23 +1,22 @@
+"use strict";
+
 const profile = {
-  username: 'Jacob',
-  playTime: 300,
-
-  changeUsername(newName) {
-    this.username = newName;
-  },
-  updatePlayTime(hours) {
-    this.playTime += hours;
-  },
-  getInfo() {
-    return `${this.username} has ${this.playTime} active hours!`;
-  },
+    username: "Jacob",
+    playTime: 300,
+    changeUsername(newName) {
+        this.username = newName;
+    },
+    updatePlayTime(hours) {
+        this.playTime += hours;
+    },
+    getInfo() {
+        return `${this.username} has ${this.playTime} active hours!`;
+    },
 };
-
-// ===== Task-3 =====
 
 console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
-profile.changeUsername('Marco');
+profile.changeUsername("Marco");
 console.log(profile.getInfo()); // "Marco has 300 active hours!"
 
 profile.updatePlayTime(20);
